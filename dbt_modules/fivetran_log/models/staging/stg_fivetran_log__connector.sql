8478a2a4d5a41fdb0902a86289d4dc81db2e2f4c
saryamane <samirarya@gmail.com>
2021-04-04 17:35:27 -0700

    Added the fivetran package files into the git repo

---
+++ dbt_modules/fivetran_log/models/staging/stg_fivetran_log__connector.sql
@@ -0,0 +1,42 @@
+with connector as (
+
+    select * from {{ var('connector') }}
+
+),
+
+fields as (
+
+    select 
+        connector_id,
+        connector_name,
+        -- connector_type,  -- use coalesce(connector_type, service) if the table has a service column (deprecated)
+        destination_id,
+        connecting_user_id,
+        paused as is_paused,
+        signed_up as set_up_at,
+
+        -- Consolidating duplicate connectors (ie deleted and then re-added)
+        row_number() over ( partition by connector_name, destination_id order by _fivetran_synced desc ) as nth_last_record
+
+    from connector
+
+),
+
+final as (
+
+    select 
+        connector_id,
+        connector_name,
+        -- connector_type,
+        destination_id,
+        connecting_user_id,
+        is_paused,
+        set_up_at
+
+    from fields
+
+    -- Only look at the most recent one
+    where nth_last_record = 1
+)
+
+select * from final
---
+++ dbt_modules/fivetran_log/models/staging/stg_fivetran_log__log.sql
@@ -0,0 +1,25 @@
+with log as (
+
+    select * from {{ var('log') }}
+    
+),
+
+fields as (
+
+    select
+        id as log_id, 
+        time_stamp as created_at,
+        connector_id, -- Note: the connector_id column used to erroneously equal the connector_name, NOT its id.
+        case when transformation_id is not null and event is null then 'TRANSFORMATION'
+        else event end as event_type, 
+        message_data,
+        case 
+        when transformation_id is not null and message_data like '%has succeeded%' then 'transformation run success'
+        when transformation_id is not null and message_data like '%has failed%' then 'transformation run failed'
+        else message_event end as event_subtype,
+        transformation_id
+
+    from log
+)
+
+select * from fields 
